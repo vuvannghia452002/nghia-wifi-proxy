784d1d550b5c0761f2c058660484f3793553785b
VuVanNghia <nghiavu2k2abc@gmail.com>
2024-05-01 09:29:26 +0700

    update main.py

--- main.py
+++ main.py
@@ -21,6 +21,21 @@ def set_proxy(proxy_address, proxy_port):
         print("Đã xảy ra lỗi:", e)
 
 
+def disable_proxy():
+    try:
+        reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
+        key = winreg.OpenKey(
+            reg, r"Software\Microsoft\Windows\CurrentVersion\Internet Settings", 0, winreg.KEY_WRITE)
+
+        winreg.SetValueEx(key, "ProxyEnable", 0, winreg.REG_DWORD, 0)
+
+        winreg.CloseKey(key)
+
+        print("Proxy đã được tắt thành công.")
+    except Exception as e:
+        print("Đã xảy ra lỗi khi tắt proxy:", e)
+
+
 wifi_output = subprocess.check_output(['netsh', 'WLAN', 'show', 'interfaces'])
 data_infor = wifi_output.decode('utf-8')
 
@@ -30,7 +45,7 @@ if name_vvn20206205 in data_infor:
     print(f"🚀 Có kết nối {name_vvn20206205}")
 else:
     print(f"🚀 Không kết nối {name_vvn20206205}")
-    # Tắt proxy       hướng dẫn              python
+    disable_proxy()
     exit()
 
 
